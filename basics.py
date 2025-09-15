cad611dd2f5612c6137d9d7d1c15ff9f0577978b
gmrp22 <gmrp22@hotmail.com>
2025-09-14 18:17:11 -0600

    basic operations

--- basics.py
+++ basics.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from math import gcd
 import numpy as np
 
 
@@ -26,3 +28,27 @@ print('row to column vector transpose:\n', transpose)
 #Addition
 v5 = v1 - v4
 print('Addition of two row vectors:\n', v5) 
+
+
+#Multiplication by scalar
+v6 = v1 * 3
+print('Multiplication by scalar:\n', v6)
+
+v1 = np.array([1, 2, 3])
+v2 = np.array([1, -2, 3])
+v3 = 4*v1 + 2*v2
+print('Linear combination of two vectors:\n', v3)
+
+#FACTORING OUT simple
+v = np.array([6, 9])
+
+# Compute GCD of all elements
+common_factor = reduce(gcd, v)
+
+# Factor out the common factor
+factored_vector = v // common_factor
+
+print("Original vector:", v)
+print("Common factor:", common_factor)
+print("Factored vector:", factored_vector)
+
