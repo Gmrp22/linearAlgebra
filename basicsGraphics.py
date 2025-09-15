342c009c8111a332af1ac5f50e8fb362ed307613
gmrp22 <gmrp22@hotmail.com>
2025-09-14 18:33:12 -0600

    plt arrpw for vectors

--- basicsGraphics.py
+++ basicsGraphics.py
@@ -13,3 +13,26 @@ plt.xlabel("Índice")
 plt.ylabel("Valor")
 plt.grid(True)
 plt.show()
+
+# plt.arrpw
+
+# Vector 1: [2, 2], starting at (0,0)
+start1 = np.array([0, 0])
+vec1 = np.array([2, 2])
+
+# Vector 2: [6, 12], starting at (1, -2)
+start2 = np.array([1, -2])
+vec2 = np.array([6, 12])
+
+plt.figure()
+# Dibujar vectores como flechas
+plt.arrow(start1[0], start1[1], vec1[0], vec1[1], head_width=0.5, head_length=0.7, color='blue', length_includes_head=True)
+plt.arrow(start2[0], start2[1], vec2[0], vec2[1], head_width=0.5, head_length=0.7, color='red', length_includes_head=True)
+
+plt.xlim(-1, 10)
+plt.ylim(-5, 15)
+plt.grid(True)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Vectores con punto de inicio")
+plt.show()
